31b4916d7c8e5ac8599f188d9111a398641baee7
Jonas Liebschner <jonas.liebschner@gmx.de>
2021-11-20 20:04:01 +0100

    add delete element in home

--- bookify/lib/screens/home.dart
+++ bookify/lib/screens/home.dart
@@ -11,19 +11,27 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<String> names = ["O2-R5-P1", "O2-R5-P1", "O2-R5-P3", "O1-R2-P3", "O2-R5-P3"];
+
+  void deleteElement(String name) {
+    setState(() {
+      names.remove(name);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
         padding: const EdgeInsets.all(8),
-        child: ListView(
-          children: [
-            MainCard(),
-            MainCard(),
-            MainCard(),
-            MainCard(),
-            MainCard(),
-            MainCard()
-          ],
+        child: ListView.builder(
+          itemCount: names.length,
+          itemBuilder: (context, index) {
+            return MainCard(
+              name: names[index],
+              parentAction: deleteElement,
+            );
+          },
         )
     );
   }
--- bookify/lib/widgets/main_card.dart
+++ bookify/lib/widgets/main_card.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class MainCard extends StatefulWidget {
-  const MainCard({Key key}) : super(key: key);
+  final void Function(String name) parentAction;
+  const MainCard({Key key, this.name, this.parentAction}) : super(key: key);
+
+  final name;
 
   @override
   _MainCardState createState() => _MainCardState();
@@ -12,43 +15,17 @@ class _MainCardState extends State<MainCard> {
   @override
   Widget build(BuildContext context) {
     return Slidable(
-      startActionPane: ActionPane(
-        motion: const ScrollMotion(),
-        children: const [
+      endActionPane: ActionPane(
+        motion: ScrollMotion(),
+        children: [
           SlidableAction(
-            onPressed: doNothing,
+            onPressed: (BuildContext context) {
+              widget.parentAction(widget.name);
+            },
             backgroundColor: Color(0xFFFE4A49),
             foregroundColor: Colors.white,
             icon: Icons.delete,
             label: 'Delete',
-          ),
-          SlidableAction(
-            onPressed: doNothing,
-            backgroundColor: Color(0xFF21B7CA),
-            foregroundColor: Colors.white,
-            icon: Icons.share,
-            label: 'Share',
-          ),
-        ],
-      ),
-
-      endActionPane: const ActionPane(
-        motion: ScrollMotion(),
-        children: [
-          SlidableAction(
-            flex: 2,
-            onPressed: doNothing,
-            backgroundColor: Color(0xFF7BC043),
-            foregroundColor: Colors.white,
-            icon: Icons.archive,
-            label: 'Archive',
-          ),
-          SlidableAction(
-            onPressed: doNothing,
-            backgroundColor: Color(0xFF0392CF),
-            foregroundColor: Colors.white,
-            icon: Icons.save,
-            label: 'Save',
           ),
         ],
       ),
@@ -58,7 +35,7 @@ class _MainCardState extends State<MainCard> {
         child: Column(
           children: [
             ListTile(
-              title: Text("Raum: O2-R5-P3"),
+              title: Text("Platz: " + widget.name),
               subtitle: Text("Gebucht am 10.11.21 von: 8:00 - 18:00"),
             ),
           ],
@@ -67,8 +44,4 @@ class _MainCardState extends State<MainCard> {
     );
   }
 }
-
-
-
-void doNothing(BuildContext context) {}
 
