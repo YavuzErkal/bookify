6b897d653f099d3ef071e7fab4ce3e2d04227894
Jonas Liebschner <jonas.liebschner@gmx.de>
2021-11-20 11:23:05 +0100

    add cam

---
+++ bookify/lib/screens/cam.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+class CamScreen extends StatefulWidget {
+  const CamScreen({Key key, this.cameras}) : super(key: key);
+
+  final List<CameraDescription> cameras;
+
+  @override
+  _CamScreenState createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+
+  int selectedCamera = 0;
+  CameraController _controller;
+  Future<void> _initializeControllerFuture;
+
+  initCamera(int cameraIndex) {
+    _controller = CameraController(widget.cameras[cameraIndex], ResolutionPreset.medium);
+
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera(selectedCamera);
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
--- bookify/lib/screens/home.dart
+++ bookify/lib/screens/home.dart
@@ -1,5 +1,3 @@
-import 'dart:html';
-
 import 'package:bookify/widgets/main_card.dart';
 import 'package:flutter/material.dart';
 
